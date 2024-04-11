0567412097d4fa900878adcd67630bc28cca4ed6
Kevin Edinger <kevin.edinger@sap.com>
2024-04-11 14:01:14 +0200

    [INTERNAL] sap.ui.rta: introduce depth for fetching bindings for reveal
    When revealing a control, the bindings of the control and children are fetched to
    check if the control would be hidden via annotations. For Container controls
    only a certain depth of children have an influence over the visibility of
    the container. This change introduces a depth parameter in the designtime
    for the reveal action.
    This new parameter is implemented for the ObjectPageSection and ObjectPageSubSection.
    
    SNOW: DINC0080529
    Change-Id: I8c8fe37b535845d57ee281161810c1cfbb40f940
    (cherry picked from commit 31a257c48d1d21e6ab661a8bfc8877ee111ce6da)
    CR-Id: 002075125800003844622025

--- src/sap.uxap/src/sap/uxap/designtime/ObjectPageSection.designtime.js
+++ src/sap.uxap/src/sap/uxap/designtime/ObjectPageSection.designtime.js
@@ -44,7 +44,8 @@ sap.ui.define(["sap/uxap/library"],
 			},
 			reveal : {
 				changeType : "unstashControl",
-				getLabel: fnGetLabel
+				getLabel: fnGetLabel,
+				depthOfRelevantBindings: 2
 			},
 			rename: function () {
 				return {
