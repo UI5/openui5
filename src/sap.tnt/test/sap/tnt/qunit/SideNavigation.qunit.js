4fb41870fcf34ad6839c64e98fbef9c96cac2b7d
Siyana Todorova <siyana.todorova@sap.com>
2025-05-06 23:23:51 +0300

    [INTERNAL] sap.tnt.SideNavigation: Invisible texts for flexible and fixed Navigation lists are added
    Accessibility related fix, which was introduced first in 1.137 and
    adds an aria-label for the Navigation Lists.
    
    JIRA: BGSOFUIRODOPI-3230
    SNOW: CS20260011383556
    Change-Id: I9b14eb9ae8655ab063091bcf38a973d46b73b0e3
    CR-Id: 002075125800000519612026

--- src/sap.tnt/test/sap/tnt/qunit/SideNavigation.qunit.js
+++ src/sap.tnt/test/sap/tnt/qunit/SideNavigation.qunit.js
@@ -476,8 +476,8 @@ sap.ui.define([
 		});
 	});
 
-	QUnit.test("Aria attributes - aria-label", function (assert) {
-
+	QUnit.test("Aria attributes - aria-label",  function (assert) {
+		const oRB = Core.getLibraryResourceBundle("sap.tnt");
 		var label = "Side navigation menu with options";
 
 		assert.notOk(this.sideNavigation.$().attr('aria-label'),  'aria-label is not set initially');
@@ -486,5 +486,7 @@ sap.ui.define([
 		Core.applyChanges();
 
 		assert.strictEqual(this.sideNavigation.$().attr('aria-label'), label, 'aria-label is as expected');
+		assert.strictEqual(this.sideNavigation.getAggregation("item").$().attr('aria-label'), oRB.getText("SIDE_NAVIGATION_FLEXIBLE_LIST_LABEL"), 'ul for flexible list should have aria-label "Primary Navigation Menu"');
+		assert.strictEqual(this.sideNavigation.getAggregation("fixedItem").$().attr('aria-label'), oRB.getText("SIDE_NAVIGATION_FIXED_LIST_LABEL"), 'ul for fixed list should have aria-label "Footer Navigation Menu"');
 	});
 });
