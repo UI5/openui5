9d250830462597240acfb479a21d80b7f9cb7cb0
Plamen Ivanov <plamen.ivanov01@sap.com>
2025-06-06 10:31:25 +0300

    [FIX] sap.uxap.ObjectPageLayout: Fix initial title rendering in FCL
    Problem: When we have OPL in FCL on big screen, the dynamic title is
    initially rendered bigger than it should.
    
    Solution: We update CSS styles accordingly after OPL rendering.
    
    SNOW: DINC0495454
    SNOW: DINC0741879
    Change-Id: I9ea414161241aab9d2561a79fb9bd6284f491bc0
    (cherry picked from commit af66701623633ff1da145f786cafc0ec582a4e40)
    CR-Id: 002075125800000145172026

--- src/sap.uxap/src/sap/uxap/ObjectPageLayout.js
+++ src/sap.uxap/src/sap/uxap/ObjectPageLayout.js
@@ -1194,6 +1194,7 @@ sap.ui.define([
 
 		if (this._hasDynamicTitle()) {
 			this.addStyleClass("sapUxAPObjectPageHasDynamicTitle");
+			this._updateMedia(iWidth, ObjectPageLayout.DYNAMIC_HEADERS_MEDIA);
 		}
 
 		if (iWidth > 0) {
