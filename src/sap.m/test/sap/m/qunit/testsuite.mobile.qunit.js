6e25c50118dfabd78720ca14af9f75c3726db795
Teodor Taushanov <teodor.taushanov@sap.com>
2025-09-01 13:55:11 +0300

    [FIX] core/Popup: Use activeElement of both light and shadow DOM to determine focus loss
    - The focus/blur event handler is registered in the light DOM area,
      therefore the event target is always an element in the light DOM area
      even when an element in the shadow DOM is focused
    
    - This makes the focus loss determination easy because we only need to
      do "contains" check in the light DOM area.
    
    - However, when elements from shadow DOM are set as extra content of a
      Popup, for example, the opener of a sap.m.Popover, we need to get the
      actual focused element from the shadow DOM to compare with the extra
      content.
    
    - The focus handling logic is updated to inspect the
      `shadowRoot.activeElement` of the event target. This allows the Popup
      to accurately determine that focus is still within its boundaries,
      preventing unwanted side effects.
    
    JIRA: BGSOFUIRODOPI-3523
    SNOW: DINC0622589
    Fixes https://github.com/SAP/ui5-webcomponents/issues/12205
    
    Change-Id: Id82348ed7b7294dab826efd1a4ad5462dcc19b9a
    CR-Id: 002075125900003392262025

--- src/sap.m/test/sap/m/qunit/testsuite.mobile.qunit.js
+++ src/sap.m/test/sap/m/qunit/testsuite.mobile.qunit.js
@@ -318,6 +318,9 @@ sap.ui.define([
 					rtl: true
 				}
 			},
+			DialogWithWebComponents: {
+				title: "QUnit Page for sap.m.Dialog containing web components"
+			},
 			DisplayListItem: {
 				title: "Test Page for sap.m.DisplayListItem"
 			},
