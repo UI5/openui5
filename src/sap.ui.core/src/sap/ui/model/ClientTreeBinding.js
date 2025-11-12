c9768c9f1a02f669d5c129f6fd3af5a2badae377
I502081 <oliwer.christ02@sap.com>
2025-11-12 17:20:35 +0100

    [FIX] sap/ui/model/ClientTreeBinding#cloneData: use a clone for this.oTreeData
    In order to correctly determine if a change is went needs to be fired
    the stored tree data and the new tree data need to be compared.
    If they both point to the same object reference, no changes are detected.
    Therefore we need to create a clone of the tree data in cloneData
    
    PS1: downport
    PS2: apply correction from change 7058941
    
    CR-Id: 002075125900004843012025
    Change-Id: I72816121789662a0ad4eb2dc5481faff77f71d40
    SNOW: DINC0701989
    (cherry picked from commit 61530344df4de95f2f2dbe795ce7a8cb51ef4935)

--- src/sap.ui.core/src/sap/ui/model/ClientTreeBinding.js
+++ src/sap.ui.core/src/sap/ui/model/ClientTreeBinding.js
@@ -7,11 +7,12 @@ sap.ui.define([
 	"./ChangeReason",
 	"./TreeBinding",
 	"sap/base/util/deepEqual",
+	"sap/base/util/deepExtend",
 	"sap/base/util/each",
 	"sap/ui/model/FilterProcessor",
 	"sap/ui/model/FilterType",
 	"sap/ui/model/SorterProcessor"
-], function(ChangeReason, TreeBinding, deepEqual, each, FilterProcessor, FilterType, SorterProcessor) {
+], function(ChangeReason, TreeBinding, deepEqual, deepExtend, each, FilterProcessor, FilterType, SorterProcessor) {
 	"use strict";
 
 	/**
@@ -64,7 +65,7 @@ sap.ui.define([
 			};
 			this.oCombinedFilter = null;
 			this.mNormalizeCache = {};
-			this.oTreeData = this.oModel._getObject(this.sPath, this.oContext);
+			this.oTreeData = this.cloneData(this.oModel._getObject(this.sPath, this.oContext));
 
 			if (aApplicationFilters) {
 				this.oModel.checkFilter(aApplicationFilters);
@@ -77,6 +78,21 @@ sap.ui.define([
 		}
 
 	});
+
+	ClientTreeBinding.CannotCloneData = Symbol("CannotCloneData");
+
+	/**
+	 * Returns a deep clone of the tree data or a symbol indicating that the given tree data cannot be cloned.
+	 *
+	 * @param {any} oTreeData
+	 *   The tree data to clone
+	 * @returns {any|sap.ui.model.ClientTreeBinding.CannotCloneData}
+	 *   A deep clone or the symbol ClientTreeBinding.CannotCloneData
+	 * @private
+	 */
+	ClientTreeBinding.prototype.cloneData = function(oTreeData) {
+		return deepExtend(Array.isArray(oTreeData) ? [] : {}, oTreeData);
+	};
 
 	/**
 	 * Return root contexts for the tree.
@@ -456,7 +472,8 @@ sap.ui.define([
 		if (this.oContext != oContext) {
 			this.oContext = oContext;
 			if (this.isRelative()) {
-				this.oTreeData = this.oModel._getObject(this.sPath, this.oContext);
+				const oTreeData = this.oModel._getObject(this.sPath, this.oContext);
+				this.oTreeData = this.cloneData(oTreeData);
 				this._fireChange({reason: ChangeReason.Context});
 			}
 		}
@@ -478,7 +495,7 @@ sap.ui.define([
 		this._mLengthsCache = {};
 
 		if (bForceUpdate || !deepEqual(this.oTreeData, oCurrentTreeData)) {
-			this.oTreeData = oCurrentTreeData;
+			this.oTreeData = this.cloneData(oCurrentTreeData);
 			this._fireChange({reason: ChangeReason.Change});
 		}
 	};
