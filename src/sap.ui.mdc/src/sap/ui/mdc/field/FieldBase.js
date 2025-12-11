35b558a850e7f60272621700f647e31339b990ad
Luca Mueller <luca.mueller01@sap.com>
2025-12-11 10:43:31 +0100

    [INTERNAL] mdc.FieldBase: do not suppress invalidation for visible property
    Issue: FieldBase#setProperty suppresses the invalidation for properties that are rendered on the content control. This includes the "visible" property. This lead to the issue that bound values for the MultiValueField's "visible" property did not update this property although the model was
    
    Fix: remove the visible property from being suppressed.
    
    SNOW: DINC0735786
    CR-Id: 002075125800004219782025
    Change-Id: Ic51b5263062db24f28bf30587238859cbca83e73
    (cherry picked from commit fc592e458805a2674db7e4a043ccc4a2c4ab211e)

--- src/sap.ui.mdc/src/sap/ui/mdc/field/FieldBase.js
+++ src/sap.ui.mdc/src/sap/ui/mdc/field/FieldBase.js
@@ -764,7 +764,7 @@ sap.ui.define([
 	};
 
 	FieldBase.prototype.setProperty = function(sPropertyName, vValue, bSuppressInvalidate) {
-
+		const aExcemptionProperties = ["width", "multipleLines", "showEmptyIndicator", "visible"];
 		// most properties are rendered from content controls. Only invalidate whole Field if needed
 		// (multipleLines mostly changed together with editMode -> update once on rendering)
 		if (sPropertyName === "editMode") {
@@ -773,7 +773,7 @@ sap.ui.define([
 			if (sOld !== FieldEditMode.Display && sOld !== FieldEditMode.EditableDisplay && vValue !== FieldEditMode.Display && vValue !== FieldEditMode.EditableDisplay) {
 				bSuppressInvalidate = true;
 			}
-		} else if (sPropertyName !== "width" && sPropertyName !== "multipleLines" && sPropertyName !== "showEmptyIndicator") {
+		} else if (!aExcemptionProperties.includes(sPropertyName)) {
 			bSuppressInvalidate = true;
 		}
 
