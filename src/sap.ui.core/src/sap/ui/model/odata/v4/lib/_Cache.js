31ca1036c63deb7f9fb284cbbd367510838cbb2e
Alexander Riess <alexander.mathias.riess@sap.com>
2025-12-15 18:08:01 +0100

    [INTERNAL] sap.ui.model.odata.v4.lib._Cache#fetchTypes: take mLateExpandSelect into account
    PS1: (cherry picked from commit ec903bdd67c46a80b8bdcda1e5c3adbdbce319b4)
    PS2: downport blues
    
    Change-Id: Ie371cba10dcb0fd997c42a3b66d5425d4d650612
    SNOW: DINC0876272
    CR-Id: 002075125800001678252026

--- src/sap.ui.core/src/sap/ui/model/odata/v4/lib/_Cache.js
+++ src/sap.ui.core/src/sap/ui/model/odata/v4/lib/_Cache.js
@@ -1122,7 +1122,7 @@ sap.ui.define([
 			aPromises = [];
 			mTypeForMetaPath = {};
 			aPromises.push(this.oRequestor.fetchType(mTypeForMetaPath, this.sMetaPath));
-			fetchExpandedTypes(this.sMetaPath, this.mQueryOptions);
+			fetchExpandedTypes(this.sMetaPath, this.mLateExpandSelect || this.mQueryOptions);
 			this.oTypePromise = SyncPromise.all(aPromises).then(function () {
 				return mTypeForMetaPath;
 			});
