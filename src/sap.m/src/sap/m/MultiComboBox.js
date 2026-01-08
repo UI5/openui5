4d4d13edbb9bb6c2bbe98e7a644517d072bd5a85
Ivanova Terzieva <nikoleta.ivanova.terzieva@sap.com>
2026-01-08 16:05:04 +0200

    [FIX] sap.m.MultiComboBox: After deletion, next focused token is announced with correct position
    So far, when a token was deleted from the MultiComboBox, the delete
    event was fired and an adjacent token was immediately focused. The
    screen reader announced the focused token with its old position, before
    the deletion was actually applied. This caused confusion for screen
    reader users, as the announced position did not correspond to the actual
    position of the token after deletion. The posinset and setsize were
    updated only after the token was removed from the DOM. With this fix,
    the focus is moved to the adjacent token only after the deletion is
    fully processed and the DOM is updated.
    
    SNOW: CS20250011113183
    CR-Id: 002075125800000318212026
    Change-Id: Iea21ee1fed1427b5f128900930f0dfb823e44a73
    (cherry picked from commit e7d6b87efb4e13cd957c2b3e9a018ecc50858163)

--- src/sap.m/src/sap/m/MultiComboBox.js
+++ src/sap.m/src/sap/m/MultiComboBox.js
@@ -2168,8 +2168,10 @@ function(
 		this._removeSelection(aTokens);
 
 		if (aItemsBeforeRemoval.length !== ListHelpers.getSelectableItems(this.getItems())) {
-			!this.isPickerDialog() && !this.isFocusInTokenizer() && this.focus();
 			this.fireChangeEvent("");
+			if (!this.isPickerDialog() && !this.isFocusInTokenizer()){
+				setTimeout(() => this.focus(), 0);
+			}
 		}
 	};
 
@@ -2180,8 +2182,6 @@ function(
 	 * @private
 	 */
 	MultiComboBox.prototype._removeSelection = function (aTokens) {
-		var oTokenizer = this.getAggregation("tokenizer");
-
 		aTokens.forEach(function (oToken) {
 			var oItem = (oToken && this._getItemByToken(oToken));
 
@@ -2202,14 +2202,15 @@ function(
 			});
 
 			oToken.destroy();
+		}, this);
 
-			if (this.getSelectedItems().length > 0) {
-				var aTokens = oTokenizer.getTokens();
-				aTokens[aTokens.length - 1].focus();
-			} else {
+		setTimeout(() => {
+			// If all tokens are removed, focus should go to the input with a little delay in order for DOM to update and proper ARIA announcement to be made
+			const oTokenizer = this.getAggregation("tokenizer");
+			if (!oTokenizer || !oTokenizer.getTokens().length) {
 				this.focus();
 			}
-		}, this);
+		}, 0);
 	};
 
 	/**
