0c58684c5d4b6ffb5de38e2b37ea7d29d673742f
Sven Bernhard <sven.bernhard@sap.com>
2026-01-12 09:35:52 +0100

    [INTERNAL] sap.ui.model.odata.v4.ODataParentBinding#destroy: mAggregatedQueryOptions may be updated after destroy
    Avoid errors if binding is destroyed before the asynchronous part of
    ODataParentBinding#fetchIfChildCanUseCache is processed.
    
    PS1: Downport to 1.142
    PS2: Resolve merge conflict in ODataParentBinding
    PS3: Remove unwanted downport in ODataParentBinding
    
    Change-Id: I00d36c68666872d8e9953097e4108648a85a5150
    SNOW: CS20260011383971
    (cherry picked from commit 34e4bc440e3b0d609bb141296d14c1437b68ee1d)
    CR-Id: 002075125800000195112026

--- src/sap.ui.core/test/sap/ui/core/qunit/odata/v4/ODataParentBinding.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/odata/v4/ODataParentBinding.qunit.js
@@ -3417,6 +3417,7 @@ sap.ui.define([
 	QUnit.test("destroy", function (assert) {
 		var oBinding = new ODataParentBinding();
 
+		oBinding.mAggregatedQueryOptions = "~mAggregatedQueryOptions~";
 		oBinding.aChildCanUseCachePromises = [{}, {}];
 		oBinding.mChildPathsReducedToParent = "~mChildPathsReducedToParent~";
 		oBinding.oResumePromise = {};
@@ -3426,7 +3427,7 @@ sap.ui.define([
 		// code under test
 		oBinding.destroy();
 
-		assert.strictEqual(oBinding.mAggregatedQueryOptions, undefined);
+		assert.deepEqual(oBinding.mAggregatedQueryOptions, {});
 		assert.deepEqual(oBinding.aChildCanUseCachePromises, []);
 		assert.deepEqual(oBinding.mChildPathsReducedToParent, {});
 		assert.strictEqual(oBinding.oRefreshPromise, undefined);
