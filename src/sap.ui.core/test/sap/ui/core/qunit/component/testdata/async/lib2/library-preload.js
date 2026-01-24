981418d7600cb1b1b9a7842d21554a0a3459c9e3
Frank Weigel <frank.weigel@sap.com>
2026-01-24 09:03:11 +0100

    [INTERNAL] Fix schema version in main-only manifests
    This is a follow-up to change I40db0f28f6b41f242b4449fb99025aa78dbb9195
    (commit d6bc2b6b687485a0519aa600661808da5514858c).
    
    Change-Id: Ia7b963b97d7ed7b688ea839e40a06c5dfcebbb9d

--- src/sap.ui.core/test/sap/ui/core/qunit/component/testdata/async/lib2/library-preload.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/component/testdata/async/lib2/library-preload.js
@@ -13,6 +13,6 @@ sap.ui.require.preload({
 	"version":"2.0",
 	"name":"sap.test.lib2",
 	"modules":{
-		"sap/test/lib2/manifest.json":"{\"sap.app\":{\"id\":\"sap.test.lib2\",\"type\":\"library\",\"title\":\"Library sap.test.lib2\",\"applicationVersion\":{\"version\":\"1.0.0\"}},\"sap.ui\":{\"technology\":\"UI5\",\"deviceTypes\":{\"desktop\":true,\"tablet\":true,\"phone\":true}},\"sap.ui5\":{\"dependencies\":{\"minUI5Version\":\"2.0.0\",\"libs\":{\"sap.test.lib4\":{}}}}}"
+		"sap/test/lib2/manifest.json":"{\"_version\":\"2.0.0\",\"sap.app\":{\"id\":\"sap.test.lib2\",\"type\":\"library\",\"title\":\"Library sap.test.lib2\",\"applicationVersion\":{\"version\":\"1.0.0\"}},\"sap.ui\":{\"technology\":\"UI5\",\"deviceTypes\":{\"desktop\":true,\"tablet\":true,\"phone\":true}},\"sap.ui5\":{\"dependencies\":{\"minUI5Version\":\"2.0.0\",\"libs\":{\"sap.test.lib4\":{}}}}}"
 	}
 });
--- src/sap.ui.core/test/sap/ui/core/qunit/component/testdata/async/lib3/library-preload.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/component/testdata/async/lib3/library-preload.js
@@ -13,6 +13,6 @@ sap.ui.require.preload({
 	"version":"2.0",
 	"name":"sap.test.lib3",
 	"modules":{
-		"sap/test/lib3/manifest.json":"{\"sap.app\":{\"id\":\"sap.test.lib3\",\"type\":\"library\",\"title\":\"Library sap.test.lib3\",\"applicationVersion\":{\"version\":\"1.0.0\"}},\"sap.ui\":{\"technology\":\"UI5\",\"deviceTypes\":{\"desktop\":true,\"tablet\":true,\"phone\":true}},\"sap.ui5\":{\"dependencies\":{\"minUI5Version\":\"2.0.0\",\"libs\":{\"sap.test.lib2\":{}}}}}"
+		"sap/test/lib3/manifest.json":"{\"_version\":\"2.0.0\",\"sap.app\":{\"id\":\"sap.test.lib3\",\"type\":\"library\",\"title\":\"Library sap.test.lib3\",\"applicationVersion\":{\"version\":\"1.0.0\"}},\"sap.ui\":{\"technology\":\"UI5\",\"deviceTypes\":{\"desktop\":true,\"tablet\":true,\"phone\":true}},\"sap.ui5\":{\"dependencies\":{\"minUI5Version\":\"2.0.0\",\"libs\":{\"sap.test.lib2\":{}}}}}"
 	}
 });
