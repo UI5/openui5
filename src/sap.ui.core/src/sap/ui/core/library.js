087b5dd9a324f60c8ab0b00bccc565e95df61ffb
Tommy Vinh Lam <tommy.vinh.lam@sap.com>
2026-03-16 14:19:38 +0100

    [INTERNAL] sap.ui.core: Add internal debugging utilities
    Adds a global `ui5` debug facade when `sap-ui-debug=true` or `sap-ui-debug-tools=true`.
    
    The `ui5` object provides convenient access to modules and their
    functions, enabling easier inspection and debugging directly
    from the browser console.
    
    JIRA: CPOUI5FRAMEWORK-885
    Change-Id: I51aa0c5c3dd7bd3375983c82772c2f192a9cb23b

--- src/sap.ui.core/src/sap/ui/core/library.js
+++ src/sap.ui.core/src/sap/ui/core/library.js
@@ -129,6 +129,7 @@ sap.ui.define([
 		  "sap.ui.core.InvisibleMessage"
 		 ],
 		 extensions: {
+			"sap.ui.debug": true,
 			 "sap.ui.support" : {
 				 diagnosticPlugins: [
 				  "sap/ui/core/support/plugins/TechInfo",
