58f9cadc860911ccd611eb688678d0a0ec84d0b2
Christoph Szabo <christoph.szabo@sap.com>
2026-05-04 09:53:47 +0200

    [INTERNAL][FIX] sap.ui.rta, sap.ui.dt, sap.ui.fl: Harden script loading in internal bootstrap scripts by replacing document.write
    Due to a security problem, document.write has to be replaced.
    
    SNOW: DINC0863605
    CR-Id: 002075125800001715272026
    Change-Id: I092dc54cab07fb7fec8393d176669514686067f6

---
+++ src/sap.ui.rta/test/sap/ui/rta/internal/testdata/embeddedComponent/sites/flpSandbox_ushell_config.js
@@ -0,0 +1,63 @@
+(function() {
+	"use strict";
+
+	globalThis["sap-ushell-config"] = {
+		defaultRenderer: "fiori2",
+		bootstrapPlugins: {
+			RuntimeAuthoringPlugin: {
+				component: "sap.ushell.plugins.rta"
+			},
+			PersonalizePlugin: {
+				component: "sap.ushell.plugins.rta-personalize"
+			}
+		},
+		renderers: {
+			fiori2: {
+				componentData: {
+					config: {
+						enableMergeAppAndShellHeaders: true,
+						search: "hidden"
+					}
+				}
+			}
+		},
+		applications: {
+			"masterDetail-display": {
+				additionalInformation: "SAPUI5.Component=sap.ui.rta.test.embeddedComponent",
+				applicationType: "URL",
+				url: "../",
+				description: "Embedded Component Test App",
+				title: "UI Flexibility",
+				applicationDependencies: {
+					self: { name: "sap.ui.rta.test.embeddedComponent" },
+					manifest: true,
+					asyncHints: {
+						libs: [
+							{ name: "sap.ui.core" },
+							{ name: "sap.m" },
+							{ name: "sap.ui.dt" },
+							{ name: "sap.ui.rta" },
+							{ name: "sap.ui.layout" },
+							{ name: "sap.ui.comp" }
+						]
+					}
+				}
+			}
+		},
+		services: {
+			NavTargetResolution: {
+				config: {
+					allowTestUrlComponentConfig: true,
+					enableClientSideTargetResolution: true
+				}
+			},
+			EndUserFeedback: {
+				adapter: {
+					config: {
+						enabled: true
+					}
+				}
+			}
+		}
+	};
+}());
