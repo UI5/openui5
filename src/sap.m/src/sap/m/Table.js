97a9696eeb5027aecaada824697f485ba9f1121d
Oliver Stoyanovski <oliver.stoyanovski@sap.com>
2026-05-05 17:54:19 +0200

    [INTERNAL][FIX] m.Table: No columns illustrated message announcement
    - Removed the double announcement of the no columns illustrated message
    - Removed the double announcement of the title for mdc.Table ResponsiveTableType
    
    SNOW: DINC0854557
    Change-Id: I69aac95335175ca133d890565a675b05ed2fd3e8
    CR-Id: 002075125800001766902026

--- src/sap.m/src/sap/m/Table.js
+++ src/sap.m/src/sap/m/Table.js
@@ -1161,7 +1161,9 @@ sap.ui.define([
 		}
 
 		ListBase.prototype.onfocusin.call(this, oEvent);
-		this._setNoColumnsMessageAnnouncement(oTarget);
+		if (oTarget.id === this.getId("nodata")) {
+			this._setNoColumnsMessageAnnouncement(oTarget);
+		}
 	};
 
 	// event listener for theme changed
