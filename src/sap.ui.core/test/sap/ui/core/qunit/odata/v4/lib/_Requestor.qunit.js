deca2cf170cb1206699b482f11516d25c523f7ce
Dr. Thomas Chadzelek <thomas.chadzelek@sap.com>
2026-07-27 20:56:31 +0200

    [INTERNAL] sap.ui.model.odata.v4.lib._Requestor#waitForBatchResponseReceived: skip empty change sets
    PS1: (cherry picked from commit eeca3dff36932d47669fe7d90fbd4a0b00bb516e)
    PS2: resolve conflicts, CR-Id
    
    Change-Id: I47dc6d7d07f49bfc13fe988bace753e6067ae035
    SNOW: DINC0940782
    CR-Id: 002075125800003278802026

--- src/sap.ui.core/test/sap/ui/core/qunit/odata/v4/lib/_Requestor.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/odata/v4/lib/_Requestor.qunit.js
@@ -4551,8 +4551,14 @@ sap.ui.define([
 			oSyncPromise;
 
 		oRequestor.mBatchQueue = {
-			myGroup : [[{$promise : Promise.resolve("~result~")}]]
+			myGroup : [
+				[],
+				[],
+				[{$promise : Promise.resolve("~result~")}, {$promise : "n/a"}],
+				[{$promise : "n/a"}]
+			]
 		};
+		oRequestor.mBatchQueue.myGroup.iChangeSet = 3;
 
 		// code under test
 		oSyncPromise = oRequestor.waitForBatchResponseReceived("myGroup");
@@ -4562,6 +4568,25 @@ sap.ui.define([
 		return oSyncPromise.then(function (vResult) {
 			assert.strictEqual(vResult, "~result~");
 		});
+	});
+
+	//*****************************************************************************************
+	QUnit.test("waitForBatchResponseReceived: no change requests yet", function (assert) {
+		const oRequestor = _Requestor.create(sServiceUrl, oModelInterface, {}, {}, "4.0");
+		oRequestor.mBatchQueue = {
+			myGroup : [
+				[],
+				[],
+				[]
+			]
+		};
+		oRequestor.mBatchQueue.myGroup.iChangeSet = 2;
+
+		assert.strictEqual(
+			// code under test
+			oRequestor.waitForBatchResponseReceived("myGroup"),
+			undefined,
+			"break the caller ;-)");
 	});
 
 	//*****************************************************************************************
