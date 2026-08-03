3bf3393234edab8ef2791970abc7007413e8fee0
Ashwin Kumar Bolar <ashwin.kumar.bolar@sap.com>
2026-08-03 12:28:23 +0530

    [INTERNAL] Handling testcase failure in PDFViewer
    SNOW: DINC0976784
    CR-Id: 002075125800003391072026
    Change-Id: Ibe4bcab29e3bff295dbf7ee8883c1ab67bc9b34b

--- src/sap.m/test/sap/m/qunit/PDFViewer.embedded.qunit.js
+++ src/sap.m/test/sap/m/qunit/PDFViewer.embedded.qunit.js
@@ -173,7 +173,7 @@ sap.ui.define([
 				if (iPhase === 0) {
 					iPhase = 1;
 					oModel.setData({ showDownloadButton: true }, true);
-					TestUtils.triggerRerender();
+					TestUtils.rerender();
 				} else if (iPhase === 1) {
 					iPhase = 2;
 					var oOverflowToolbar = oPdfViewer.$('overflowToolbar');
@@ -186,7 +186,7 @@ sap.ui.define([
 					assert.ok(oDownloadButton.length === 1, 'Download button should be visible');
 
 					oModel.setData({ showDownloadButton: false }, true);
-					TestUtils.triggerRerender();
+					TestUtils.rerender();
 				} else if (iPhase === 2) {
 					var oOverflowToolbar = oPdfViewer.$('overflowToolbar');
 					assert.ok(oOverflowToolbar.length === 0, "OverflowToolbar should be hidden");
