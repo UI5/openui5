7671942fa4779c9121fb5215f66c833b2287c918
Siyana Todorova <siyana.todorova@sap.com>
2026-09-03 10:43:04 +0300

    [FIX] sap.m.Title: visual test is now linked to control
    The newly added visual test TitleInSimpleForm was missing the suite meta
    for control name linking it to the sap.m.Title.
    
    Change-Id: Ie863902b5953dfc37768d0c24a1124fab4532a1c

--- src/sap.m/test/sap/m/visual/TitleInSimpleForm.spec.js
+++ src/sap.m/test/sap/m/visual/TitleInSimpleForm.spec.js
@@ -1,7 +1,9 @@
-/*global describe,it,element,by,takeScreenshot,expect*/
+/*global describe,it,element,by,takeScreenshot,expect,browser*/
 
 describe("sap.m.TitleInSimpleForm", function() {
 	"use strict";
+
+	browser.testrunner.currentSuite.meta.controlName = "sap.m.Title";
 
 	// initial loading - all three form layouts
 	it("should load test page", function () {
